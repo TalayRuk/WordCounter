0743fcc3698208db53bc4fe93892ac02e068c174
Talayruk <notejde@gmail.com>
2016-09-16 23:57:28 -0700

    add Test1 and Obj

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,9 +1,20 @@
 using Xunit;
-// using ADDnamespace.Objects;
+using WordCounter.Objects
 using System;
 using System.Collections.Generic;
 
-namespace //ADDnamespace
+namespace WordCounter
 {
+  public class RepeatCounter
+  {
+    [Fact]
+    public void Test1_CounterRepeats_Are2OutputsSame_No()
+    {
+      WordsCount testCount = new WordsCount(and, pet)
+      testCount.CouterRepeats();
+      int newcount = 0;
+      Assert.Equal( newcount, testCount.GetResult() );
+    }
+  }
 
 }
